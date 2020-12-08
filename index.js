1dc0a89f5dbf6c17d7ca42d67b46bc1c6abf0a02
Mitchell Youngerman (Aquent LLC) <v-myoung@microsoft.com>
2020-12-08 13:43:06 -0800

    Rewrite foundation of the program

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-const fs = require('fs');
+//let submitBtn = document.getElementById('submitBtn');
+/* let filesArr = 0; */
+
+/* submitBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    let files = document.getElementById('myFile').files;
+    numFiles = files.length;
+    console.log(`You selected ${numFiles} files.`);
+
+    filesArr = files;
+
+    for (let i = 0; i < files.length; i++) {
+    // For each file, read the file contents, convert text as needed, and then save the file.
+      console.log(files[i]);
+    }
+
+});
+ */
+
+let htmlToConvert = '';
+let convertBtn = document.getElementById('convertBtn');
+
+function removeTags(html) {
+  // list of tags we should keep
+  const tagsToKeep = [
+    'a',
+    'title',
+    'h1',
+    'h2',
+    'h3',
+    'h4',
+    'ul',
+    'ol',
+    'li',
+    'strong'
+  ];
+}
+
+convertBtn.addEventListener('click', () => {
+  htmlToConvert = document.getElementById('htmlContent').value;
+  removeTags(htmlToConvert);
+});
