79020c23d9411e715ba0d97df7e60afb9ec434ec
Mitchell Youngerman (Aquent LLC) <v-myoung@microsoft.com>
2020-12-11 08:38:20 -0800

    Continue dev

--- index.js
+++ index.js
@@ -36,44 +36,55 @@ function checkTags(html) {
     'li',
     'strong'
   ];
+  let firstSearch = true;
+  let lastIndexChecked = 0;
 
   removeTags(html);
-}
 
-function removeTags(html) {
-  /* search html for '<'. Mark that index. From that index,
-  search for '>'. 
-  */
-
-  let stringStart = html.indexOf('<');
-  let stringEnd = html.indexOf('>', stringStart);
-  let wholeTag = '';
-  let deleteTag = true;
-
-  for (let i = stringStart; i <= stringEnd; i++) {
-    // get the entire string between the two indices
-    wholeTag += html[i];
-  }
-
-  // once we have the whole string, see if we should keep or erase it
-  tagsToKeep.forEach((el) => {
-    if (wholeTag.includes(`<${el}`)) {
-      deleteTag = false;
+  function removeTags(html) {
+    /* search html for '<'. Mark that index. From that index,
+    search for '>'. 
+    */
+  
+    if (firstSearch === false) {
+      var stringStart = html.indexOf('<', lastIndexChecked);
+    } else {
+      var stringStart = html.indexOf('<');
     }
-  });
-
-  if (deleteTag === true) {
-    convertedText = html.replace(wholeTag, '');
+    let stringEnd = html.indexOf('>', stringStart);
+    let wholeTag = '';
+    let deleteTag = true;
+  
+    for (let i = stringStart; i <= stringEnd; i++) {
+      // get the entire string between the two indices
+      wholeTag += html[i];
+    }
+  
+    // once we have the whole string, see if we should keep or erase it
+    tagsToKeep.forEach((el) => {
+      if (wholeTag.includes(`<${el}`)) {
+        deleteTag = false;
+      }
+    });
+  
+    if (deleteTag === true) {
+      convertedText = html.replace(wholeTag, '');
+    }
+  
+    // loop again if there's more < in the html after the current index
+    if (html.indexOf('<', stringEnd)) {
+      console.log('looking for more tags');
+      lastIndexChecked = stringEnd;
+      firstSearch = false;
+      removeTags(html);
+    } else {
+    // show the converted content in the Markdown box
+    document.getElementById('markdownContent').value = convertedText;
+    }
   }
-
-  // loop again if there's more < in the html after the current index
-  if (html.indexOf('<', stringEnd)) {
-    removeTags();
-  } else {
-  // show the converted content in the Markdown box
-  document.getElementById('markdownContent').value = convertedText;
-  }
+  
 }
+
 
 convertBtn.addEventListener('click', () => {
   htmlToConvert = document.getElementById('htmlContent').value;
